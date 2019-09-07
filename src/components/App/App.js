57b16296cb5f8c2b001674f2c15e1f8bff541a5d
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-09-07 14:14:04 +0400

    Create components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Jumbotron } from "reactstrap";
+import Footer from "../Footer/Footer";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Jumbotron>
-        <header className="App-header">
-          <h1>Welcome</h1>
-        </header>
-      </Jumbotron>
-    </div>
+    <>
+      <div className="App">
+        <Jumbotron>
+          <header className="App-header"></header>
+        </Jumbotron>
+      </div>
+      <Footer />
+    </>
   );
 }
 
