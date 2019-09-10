b073d6b4e1b30b8120ad91ea0aedcd8af1ad3e8b
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-09-10 11:11:17 +0400

    Add sections in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,28 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer">
-      <p className="text-center">Copyright &copy; {new Date().getFullYear()}</p>
+      <div className="footer-section">
+        <div>
+          <h4>Mauritius</h4>
+          <p>Mauritius Mauritius Mauritius Mauritius</p>
+        </div>
+        <div>
+          <h4>Contact</h4>
+          <p>Contact Mauritius Mauritius</p>
+        </div>
+        <div>
+          <h4>Social media</h4>
+          <p>Mauritius Mauritius</p>
+        </div>
+        <div>
+          <h4>Subscribe</h4>
+          <p>Mauritius Mauritius</p>
+        </div>
+      </div>
+      <p className="text-center">
+        <span>Copyright &copy;</span>
+        <span>{new Date().getFullYear()}</span>
+      </p>
     </div>
   );
 }
