d03ffd18f0dffc64c58c4709d22268e03011f824
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-09-10 11:12:26 +0400

    Create popular places component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Carousel from "../Carousel/Carousel";
 import Navbar from "../Navbar/Navbar";
+import PopularPlaces from "../PopularPlaces/PopularPlaces";
 
 function Home() {
   return (
     <div className="home">
       <Navbar />
       <Carousel />
+      <PopularPlaces />
     </div>
   );
 }
