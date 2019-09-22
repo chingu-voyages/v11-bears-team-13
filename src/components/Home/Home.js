f7c3733d6016110c05fb323fb093475d1664fe80
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-09-22 20:53:30 +0530

    Rename Carousel component name

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Carousel from "../Carousel/Carousel";
+import HeroImageSlider from "../HeroImageSlider/HeroImageSlider";
 import PopularPlaces from "../PopularPlaces/PopularPlaces";
 
 function Home() {
   return (
     <div className="home">
-      <Carousel />
+      <HeroImageSlider />
       <PopularPlaces />
     </div>
   );
