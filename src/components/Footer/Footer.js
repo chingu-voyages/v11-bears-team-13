1fa34bcb4158ce04942e0ef048923efe39493507
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-09-22 20:51:37 +0530

    Update footer layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,28 +1,73 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faFacebookF,
+  faInstagram
+} from "@fortawesome/fontawesome-free-brands";
 import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <div className="footer-section">
+      <div className="footer-container">
         <div>
-          <h4>Mauritius</h4>
-          <p>Mauritius Mauritius Mauritius Mauritius</p>
+          <h4>
+            <span
+              role="img"
+              aria-label="palm tree"
+              style={{ marginRight: "0.5rem" }}
+            >
+              &#x1F334;
+            </span>
+            Traverse Mauritius
+          </h4>
+          <p>Mauritius Travel Guide</p>
         </div>
+
         <div>
-          <h4>Contact</h4>
-          <p>Contact Mauritius Mauritius</p>
-        </div>
-        <div>
-          <h4>Social media</h4>
-          <p>Mauritius Mauritius</p>
+          <h4>Follow us</h4>
+          <ul className="social-links">
+            <li>
+              <FontAwesomeIcon
+                icon={faFacebookF}
+                size="2x"
+                style={{ color: "#E47600" }}
+              />
+            </li>
+            <li>
+              <FontAwesomeIcon
+                icon={faInstagram}
+                size="2x"
+                style={{ color: "#E47600" }}
+              />
+            </li>
+            <li>
+              <FontAwesomeIcon
+                icon={faTwitter}
+                size="2x"
+                style={{ color: "#E47600" }}
+              />
+            </li>
+          </ul>
         </div>
         <div>
           <h4>Subscribe</h4>
-          <p>Mauritius Mauritius</p>
+          <form className="form-inline mx-2 subscribe-form">
+            <label htmlFor="subscribe" style={{ margin: "0 auto" }}>
+              <input
+                className="form-control"
+                style={{ maxWidth: "280px" }}
+                type="email"
+                placeholder="E-mail"
+                aria-label="subscribe"
+                id="subscribe"
+              />
+            </label>
+          </form>
         </div>
       </div>
-      <p className="text-center">
+      <p className="text-center copy-right">
         <span>Copyright &copy;</span>
         <span>{new Date().getFullYear()}</span>
       </p>
